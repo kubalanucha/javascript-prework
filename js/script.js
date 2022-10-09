da0ba232257b3505f3e666c1e5d4391dd3dec689
Kuba Lanucha <kubalanucha@gmail.com>
2022-10-09 12:24:11 +0200

    Add result

--- js/script.js
+++ js/script.js
@@ -29,3 +29,25 @@ if (playerInput == '1') {
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+  printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+  printMessage('Ty przegrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'papier') {
+  printMessage('remis!');
+} else if (computerMove == 'kamień' && playerMove == 'kamień') {
+  printMessage('remis!');
+} else if (computerMove == 'nożyce' && playerMove == 'nożyce') {
+  printMessage('remis!');
+} else {
+  printMessage('nieznany ruch');
+}
