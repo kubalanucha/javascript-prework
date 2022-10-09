94dab6fc907ae3f4af2cba544194f59fe124d44f
Kuba Lanucha <kubalanucha@gmail.com>
2022-10-09 11:36:53 +0200

    Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function printMessage(msg) {
-  let div = document.createElement('div');
-  div.innerHTML = msg;
-  document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == 1) {
+  computerMove = 'kamień';
+} else if (randomNumber == 2) {
+  computerMove = 'papier';
+} else if (randomNumber == 3) {
+  computerMove = 'nożyce';
 }
 
-function clearMessages() {
-  document.getElementById('messages').innerHTML = '';
-}
-
-let playerMove = `papier`;
-
-printMessage(
-  'Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!'
-);
+printMessage('Mój ruch to: ' + computerMove);
